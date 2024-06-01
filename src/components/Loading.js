919c4eeadbcc1a3a1e01f2572dd771928d1921b7
charleysmithschofield <charleysmithschofield@gmail.com>
2024-05-31 23:25:09 -0700

    add Loading and Panels class components

---
+++ src/components/Loading.js
@@ -0,0 +1,8 @@
+// Loading.js
+import React, { Component } from "react";
+
+export default class Loading extends Component {
+ render() {
+  return <section className="loading">Loading</section>;
+ }
+}
